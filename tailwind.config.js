aff0822a38924eb08f946496fc3082846be7295d
Ganesh Dandekar <ganesh15122002@gmail.com>
2024-12-25 20:59:02 +0530

    updates 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+import lineClamp from "@tailwindcss/line-clamp";
+
 export default {
   content: ["./index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require("@tailwindcss/line-clamp")],
+  plugins: [daisyui, lineClamp],
 }
 
