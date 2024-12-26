30b1345175767130d990c8705afb0cb1cd1a700e
Ganesh Dandekar <ganesh15122002@gmail.com>
2024-12-26 23:13:08 +0530

    update

--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -6,7 +6,7 @@ import axiosInstance from '../../Helpers/axiosInstance';
 const initialState = {
     isLoggedIn: localStorage.getItem('isLoggedIn') || false,
     role: localStorage.getItem('role') || "",
-    data: localStorage.getItem('data') !== undefined && localStorage.getItem('data') !== null ? JSON.parse(localStorage.getItem('data')) : {}
+    data: localStorage?.getItem('data') !== undefined && localStorage?.getItem('data') !== null ? JSON?.parse(localStorage?.getItem('data')) : {}
 };
 
 
